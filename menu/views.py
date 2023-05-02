0e678ab07cc68ab0d8bae087879521b8ea518245
MonSteroids <112619594+MonSteroids@users.noreply.github.com>
2023-05-02 15:22:29 +0000

    Testing, added 3 test pizzas to Menu

--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+pizzas = [
+    {'name': 'Margarita','price': 2390},
+    {'name': 'Pepperoni','price': 2890},
+    {'name': 'Hawaiian','price': 2890}
+]
 
 def index(request):
-    return render(request, 'menu/index.html')
+    return render(request, 'menu/index.html', context={ 'pizzas': pizzas })
