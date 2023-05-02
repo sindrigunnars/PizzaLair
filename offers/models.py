cdef6ca6d3f456f6ce8ba186a3852d98c0b39f77
sindrigunnars <102031392+sindrigunnars@users.noreply.github.com>
2023-05-02 19:55:19 +0000

    more populate + fixes

--- offers/models.py
+++ offers/models.py
@@ -4,7 +4,7 @@ from menu.models import Pizza
 
 class Offer(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    price = models.FloatField()
-    description = models.CharField(max_length=255, blank=True)
-    image = models.CharField(max_length=9999, blank=True)
+    price = models.FloatField(blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    image = models.CharField(max_length=9999, blank=True, null=True)
     items = models.ManyToManyField(Pizza)
