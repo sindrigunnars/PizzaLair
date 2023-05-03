197e1da3da0139e25a454aa7baa08ac3cdb27c57
sindrigunnars <102031392+sindrigunnars@users.noreply.github.com>
2023-05-03 10:01:53 +0000

    menu images

--- menu/models.py
+++ menu/models.py
@@ -15,17 +15,17 @@ class PizzaType(models.Model):
         return self.name
 
 
+class PizzaImage(models.Model):
+    image = models.CharField(max_length=9999)
+
+
 class Pizza(models.Model):
     name = models.CharField(max_length=255, unique=True)
     price = models.FloatField()
     type = models.ForeignKey(PizzaType, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.CharField(max_length=255, blank=True)
     toppings = models.ManyToManyField(Topping)
+    images = models.ManyToManyField(PizzaImage)
 
     def __str__(self):
         return f"{self.name} - {self.price}"
-
-
-class PizzaImage(models.Model):
-    image = models.CharField(max_length=9999)
-    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
