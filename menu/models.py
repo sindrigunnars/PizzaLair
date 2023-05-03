c7a3f3a23863a2805a18a859f02ec856f7fe6cc3
sindrigunnars <102031392+sindrigunnars@users.noreply.github.com>
2023-05-03 09:14:00 +0000

    str og view í menu

--- menu/models.py
+++ menu/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Topping(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class PizzaType(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Pizza(models.Model):
@@ -15,6 +21,9 @@ class Pizza(models.Model):
     type = models.ForeignKey(PizzaType, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.CharField(max_length=255, blank=True)
     toppings = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
 
 
 class PizzaImage(models.Model):
--- menu/views.py
+++ menu/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from menu.models import Pizza
 
 # Create your views here.
 pizzas = [
-    {'name': 'Margarita','price': 2390},
-    {'name': 'Pepperoni','price': 2890},
-    {'name': 'Hawaiian','price': 2890}
+    {'name': 'Margarita', 'price': 2390},
+    {'name': 'Pepperoni', 'price': 2890},
+    {'name': 'Hawaiian', 'price': 2890}
 ]
 
+
 def index(request):
-    return render(request, 'menu/index.html', context={ 'pizzas': pizzas })
+    context = {'pizzas': Pizza.objects.all()}
+    return render(request, 'menu/index.html', context)
