a0e9ff7edfe341eb72fac7767e95f6cee22efabc
sindrigunnars <102031392+sindrigunnars@users.noreply.github.com>
2023-05-04 11:14:18 +0000

    footer and info added, styling left

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='Home Page'),
+    path('about_us/', views.about_us, name='About Us'),
+    path('locations/', views.locations, name='Locations'),
+    path('contact_us/', views.contact_us, name='Contact Us'),
 ]
--- frontpage/views.py
+++ frontpage/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from frontpage.models import *
 
-
-# Create your views here.
 
 def index(request):
     return render(request, 'frontpage/index.html')
+
+
+def about_us(request):
+    context = {
+        "information": Information.objects.get(pk=1)
+    }
+    return render(request, 'frontpage/about_us.html', context)
+
+
+def locations(request):
+    context = {
+        "information": Information.objects.get(pk=1)
+    }
+    return render(request, 'frontpage/locations.html', context)
+
+def contact_us(request):
+    context = {
+        "information": Information.objects.get(pk=1)
+    }
+    return render(request, 'frontpage/contact_us.html', context)
