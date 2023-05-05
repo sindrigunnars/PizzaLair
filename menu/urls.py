49c04865adb9c1a521da51b6a4d4c44edd3e04ae
MonSteroids <112619594+MonSteroids@users.noreply.github.com>
2023-05-05 10:53:50 +0000

    view details partial view

--- menu/urls.py
+++ menu/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='menu-index'),
     path('search/', views.search, name='search'),
+    path('<int:id>', views.get_pizza_by_id, name="pizza-details")
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from menu.models import Pizza
 
 # Create your views here.
-pizzas = [
-    {'name': 'Margarita', 'price': 2390},
-    {'name': 'Pepperoni', 'price': 2890},
-    {'name': 'Hawaiian', 'price': 2890}
-]
+# pizzas = [
+#     {'name': 'Margarita', 'price': 2390},
+#     {'name': 'Pepperoni', 'price': 2890},
+#     {'name': 'Hawaiian', 'price': 2890}
+# ]
 
 
 def index(request):
-    context = {'pizzas': Pizza.objects.all()}
+    context = {'pizzas': Pizza.objects.all().order_by('name')}
     return render(request, 'menu/index.html', context)
 
+def get_pizza_by_id(request, id):
+    return render(request, 'menu/single_pizza.html', {
+        'pizza': get_object_or_404(Pizza, pk=id)
+    })
 
 def search(request):
     search = request.GET.get('q')
