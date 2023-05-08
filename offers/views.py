63d7fe023944ddf8764882bd22a6a140033004b0
MonSteroids <112619594+MonSteroids@users.noreply.github.com>
2023-05-08 11:51:30 +0000

    partial view added, offers

--- offers/views.py
+++ offers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from offers.models import Offer
 
@@ -7,5 +7,10 @@ from offers.models import Offer
 
 
 def index(request):
-    context = {'offers': Offer.objects.all()}
+    context = {'offers': Offer.objects.all().order_by('name')}
     return render(request, 'offers/index.html', context)
+
+def get_offer_by_id(request, id):
+    return render(request, 'offers/add_to_cart.html', {
+        'offer': get_object_or_404(Offer, pk=id)
+    })
