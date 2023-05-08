d236d71606d28c4dc1585bb50efbab57ea9b0a51
MonSteroids <112619594+MonSteroids@users.noreply.github.com>
2023-05-08 13:05:17 +0000

    trying to add pizza.id to offers

--- offers/views.py
+++ offers/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render, get_object_or_404
-
-from offers.models import Offer
+from menu.models import Pizza
+from .models import Offer
 
 
 # Create your views here.
 
 
 def index(request):
-    context = {'offers': Offer.objects.all().order_by('name')}
+    context = {
+        'offers': Offer.objects.all().order_by('name'),
+        'pizzas': Pizza.objects.all()
+    }
     return render(request, 'offers/index.html', context)
 
 def get_offer_by_id(request, id):
     return render(request, 'offers/add_to_cart.html', {
         'offer': get_object_or_404(Offer, pk=id)
+    })
+
+def get_pizza_by_id(request, id):
+    return render(request, 'offers/add_to_cart.html', {
+        'pizza': get_object_or_404(Pizza, pk=id)
     })
