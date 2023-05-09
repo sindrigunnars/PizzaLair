c733c80d0a384bcd4ff6b16fbe6d678b8279126c
MonSteroids <112619594+MonSteroids@users.noreply.github.com>
2023-05-09 11:18:57 +0000

    offer, url fix

--- offers/urls.py
+++ offers/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path('', views.index, name='offers-index'),
     path('cart', views.add_offer_to_cart, name='add_offer_to_cart'),
-    path('<int:id>/', views.get_offer_by_id, name="offer-details"),  # Updated pattern
+    path('<int:id>/', views.get_offer_by_id, name="offer-details"),
+    path('cart', views.add_offer_to_cart, name='add_offer_to_cart')
 ]
